1548890e397f1160da857413fab542a28c7286d5
oreorz <37884264+oreorz@users.noreply.github.com>
2020-11-22 00:30:00 -0500

    Create Contains Duplicates.java

---
+++ Contains Duplicates.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hash = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(hash.contains(nums[i])){
+                return true;
+            } else{
+                hash.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
